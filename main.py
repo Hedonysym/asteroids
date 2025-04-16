8c139c0e9b77560b0bd757dead4cd665875c0703
Hedonysym <dallinhad123@gmail>
2025-04-16 18:17:15 -0500

    Groups made, player movement finished

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ from player import *
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 dt = 0
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
 player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
 def main():
     print(f"Starting Asteroids!\n"
@@ -22,10 +26,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        dt = clock.tick(60) / 1000.0
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen) 
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick()
+       
+       
 
 
 # ensures this wont run unless its executed directly
--- player.py
+++ player.py
@@ -18,3 +18,23 @@ class Player(CircleShape):
     # player class method override
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+    
+    # movement time
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+    
+    # self updating
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(dt * -1)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
