06621022ebfe23398ac390a1ca99be065009d05f
Hedonysym <dallinhad123@gmail>
2025-04-16 20:41:28 -0500

    hehe penus

--- player.py
+++ player.py
@@ -8,17 +8,21 @@ class Player(CircleShape):
         self.timer = 0
 
     # in the player class
-    def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
-        return [a, b, c]
+    def rectangle(self):
+        pass
     
     # player class method override
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 2
+        a = self.position + forward * self.radius + right
+        b = self.position + forward * self.radius - right 
+        c = self.position - forward * self.radius - right
+        d = self.position - forward * self.radius + right
+        rect = [a, b, c, d]
+        pygame.draw.polygon(screen, "white", rect, 2)
+        pygame.draw.circle(screen, "white", rect[2], self.radius/2.5)
+        pygame.draw.circle(screen, "white", rect[3], self.radius/2.5)
     
     # movement time
     def rotate(self, dt):
